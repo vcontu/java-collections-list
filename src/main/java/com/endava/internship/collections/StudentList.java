a9be12d5d6dca50889d84c3325173af8a9006043
Viorel Contu <Viorel.Contu@endava.com>
2021-03-29 21:59:47 +0300

    initial commit for Java Collections List Homework

---
+++ src/main/java/com/endava/internship/collections/StudentList.java
@@ -0,0 +1,145 @@
+package com.endava.internship.collections;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class StudentList implements List<Student> {
+
+    @Override
+    public int size() {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public Iterator<Student> iterator() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] ts) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public boolean add(Student student) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        //TODO
+    }
+
+    @Override
+    public Student get(int i) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student set(int i, Student student) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void add(int i, Student student) {
+        //TODO
+    }
+
+    @Override
+    public Student remove(int i) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Student> listIterator() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ListIterator<Student> listIterator(int i) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public List<Student> subList(int i, int i1) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Student> collection) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        //Ignore this for homework
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends Student> collection) {
+        //Ignore this for homework
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        //Ignore this for homework
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        //Ignore this for homework
+        throw new UnsupportedOperationException();
+    }
+}
